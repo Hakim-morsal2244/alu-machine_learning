1093a9b2f5ebb64c21517d772fdeb7b5cb3485ac
Hakim Morsal <m.hakim@alustudent.com>
2026-01-21 13:23:35 -0800

    Add task 14 solution: saddle up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to perform matrix multiplication using numpy.
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Multiply two numpy matrices and return the result.
+    """
+    return np.matmul(mat1, mat2)
