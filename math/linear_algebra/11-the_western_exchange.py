67a60418c5dd4de00e9fa446e872b13ec5e2ff01
Hakim Morsal <m.hakim@alustudent.com>
2026-01-21 13:11:58 -0800

    Add task 11 solution: the western exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to transpose a numpy.ndarray.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transpose a numpy.ndarray and return the new array.
+    """
+    return matrix.T
