e9378340bc249b1f6c9ff4eaa84e8d8947d449cf
Hakim Morsal <m.hakim@alustudent.com>
2026-01-21 13:07:56 -0800

    10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to return the shape of a numpy.ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Return the shape of a numpy.ndarray as a tuple of ints.
+    """
+    return matrix.shape
