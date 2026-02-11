eb80fae76ad0309104886d3ca1b06ceb296fa955
Hakim Morsal <m.hakim@alustudent.com>
2026-02-11 11:27:44 -0800

    Task 9: Summation of i squared

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 9: summation_i_squared"""
+
+def summation_i_squared(n):
+    """Return sum of squares from 1 to n"""
+    if type(n) != int or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
