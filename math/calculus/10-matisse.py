d99f57670db9256b5a91d169eb10fcf625240a30
Hakim Morsal <m.hakim@alustudent.com>
2026-02-11 11:43:03 -0800

    Fix Task 9 and 10: pycodestyle and test cases

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Task 10: poly_derivative"""
 
+
 def poly_derivative(poly):
-    """Return derivative of polynomial as list"""
-    if type(poly) != list or not all(isinstance(x, (int, float)) for x in poly):
+    """Return derivative of polynomial as a list"""
+    if (not isinstance(poly, list)
+            or not all(isinstance(x, (int, float)) for x in poly)):
         return None
     if len(poly) <= 1:
         return [0]
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Task 9: summation_i_squared"""
 
+
 def summation_i_squared(n):
     """Return sum of squares from 1 to n"""
     if type(n) != int or n < 1:
