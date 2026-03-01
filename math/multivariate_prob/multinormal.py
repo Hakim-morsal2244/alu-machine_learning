be05ecdead5fca2949982006becdf6f246a87165
Hakim Morsal <m.hakim@alustudent.com>
2026-02-28 16:43:12 -0800

    Fix issue

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
 """
-Module that defines the MultiNormal class representing a Multivariate Normal distribution
+Module that defines the MultiNormal class representing
+a Multivariate Normal distribution.
 """
 import numpy as np
 
 
 class MultiNormal:
     """
-    Represents a Multivariate Normal distribution
+    Represents a Multivariate Normal distribution.
     """
 
     def __init__(self, data):
         """
-        Initializes the MultiNormal distribution
+        Initializes the MultiNormal distribution.
 
-        Parameters:
-        data (numpy.ndarray): shape (d, n) data set
+        Parameters
+        ----------
+        data : numpy.ndarray of shape (d, n)
+            The data set where n is the number of data points
+            and d is the number of dimensions.
 
-        Public instance variables:
-        mean (numpy.ndarray): shape (d, 1) mean of the data
-        cov (numpy.ndarray): shape (d, d) covariance matrix of the data
+        Raises
+        ------
+        TypeError
+            If data is not a 2D numpy.ndarray.
+        ValueError
+            If n < 2.
+
+        Public instance variables
+        -------------------------
+        mean : numpy.ndarray of shape (d, 1)
+            Mean of the data.
+        cov : numpy.ndarray of shape (d, d)
+            Covariance matrix of the data.
         """
         if not isinstance(data, np.ndarray):
             raise TypeError("data must be a 2D numpy.ndarray")
@@ -36,5 +50,5 @@ class MultiNormal:
         # Center the data
         X_centered = data - self.mean
 
-        # Calculate covariance manually (d x d)
+        # Compute covariance manually (d x d)
         self.cov = np.dot(X_centered, X_centered.T) / (n - 1)
