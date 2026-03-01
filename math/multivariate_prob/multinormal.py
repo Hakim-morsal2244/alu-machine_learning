507e9a16dc4f72a0327da16ea1ba4de20fb8fedf
Hakim Morsal <m.hakim@alustudent.com>
2026-02-28 16:36:36 -0800

    Task 2

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module that defines the MultiNormal class representing a Multivariate Normal distribution
+"""
+import numpy as np
+
+
+class MultiNormal:
+    """
+    Represents a Multivariate Normal distribution
+    """
+
+    def __init__(self, data):
+        """
+        Initializes the MultiNormal distribution
+
+        Parameters:
+        data (numpy.ndarray): shape (d, n) data set
+
+        Public instance variables:
+        mean (numpy.ndarray): shape (d, 1) mean of the data
+        cov (numpy.ndarray): shape (d, d) covariance matrix of the data
+        """
+        if not isinstance(data, np.ndarray):
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+
+        d, n = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
+        # Calculate mean (d x 1)
+        self.mean = np.mean(data, axis=1, keepdims=True)
+
+        # Center the data
+        X_centered = data - self.mean
+
+        # Calculate covariance manually (d x d)
+        self.cov = np.dot(X_centered, X_centered.T) / (n - 1)
