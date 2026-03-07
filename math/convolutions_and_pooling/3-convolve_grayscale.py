7dd86460983f32dcbfb9936519617d1d40564658
Hakim Morsal <m.hakim@alustudent.com>
2026-03-07 08:40:51 -0800

    Task 3: Strided convolution fixed

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,45 +1,65 @@
 #!/usr/bin/env python3
-"""Performs a convolution on grayscale images with stride."""
+"""Performs strided convolution on grayscale images."""
 
 import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """Performs a convolution on grayscale images."""
+    """
+    Performs a convolution on grayscale images with stride.
+
+    Parameters
+    ----------
+    images : numpy.ndarray
+        (m, h, w) array of m grayscale images
+    kernel : numpy.ndarray
+        (kh, kw) convolution kernel
+    padding : str or tuple
+        'same', 'valid', or (ph, pw)
+    stride : tuple
+        (sh, sw) stride for height and width
+
+    Returns
+    -------
+    numpy.ndarray
+        Convolved images
+    """
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
 
+    # Determine padding
     if padding == 'same':
-        ph = int(((h - 1) * sh + kh - h) / 2)
-        pw = int(((w - 1) * sw + kw - w) / 2)
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
         ph, pw = padding
 
+    # Pad images
     padded = np.pad(
         images,
         ((0, 0), (ph, ph), (pw, pw)),
         mode='constant'
     )
 
-    h_out = (h + 2 * ph - kh) // sh + 1
-    w_out = (w + 2 * pw - kw) // sw + 1
+    # Output dimensions
+    out_h = (padded.shape[1] - kh) // sh + 1
+    out_w = (padded.shape[2] - kw) // sw + 1
 
-    output = np.zeros((m, h_out, w_out))
+    # Initialize output
+    output = np.zeros((m, out_h, out_w))
 
-    for i in range(h_out):
-        for j in range(w_out):
+    # Convolve using two loops
+    for i in range(out_h):
+        for j in range(out_w):
             h_start = i * sh
             h_end = h_start + kh
             w_start = j * sw
             w_end = w_start + kw
 
-            region = padded[
-                :, h_start:h_end, w_start:w_end
-            ]
-
+            region = padded[:, h_start:h_end, w_start:w_end]
             output[:, i, j] = np.sum(
                 region * kernel[np.newaxis, :, :],
                 axis=(1, 2)
