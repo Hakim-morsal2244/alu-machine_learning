5cf925c5711b003763b10305a6c404f67cc49814
Hakim Morsal <m.hakim@alustudent.com>
2026-03-07 08:32:40 -0800

    task 3 strided convolution

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,72 +1,47 @@
 #!/usr/bin/env python3
-"""Perform strided convolution on grayscale images."""
+"""Performs a convolution on grayscale images with stride."""
 
 import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """
-    Perform a strided convolution on grayscale images.
-
-    Parameters
-    ----------
-    images : numpy.ndarray
-        Numpy array of shape (m, h, w) with m grayscale images
-    kernel : numpy.ndarray
-        Numpy array of shape (kh, kw) with the convolution kernel
-    padding : str or tuple
-        'same', 'valid', or tuple (ph, pw) for custom padding
-    stride : tuple
-        Tuple (sh, sw) specifying the stride for height and width
-
-    Returns
-    -------
-    numpy.ndarray
-        Numpy array containing the convolved images
-    """
+    """Performs a convolution on grayscale images."""
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
 
-    # Determine padding
     if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = int(((h - 1) * sh + kh - h) / 2)
+        pw = int(((w - 1) * sw + kw - w) / 2)
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
         ph, pw = padding
 
-    # Only pad if needed
-    if ph > 0 or pw > 0:
-        padded_images = np.pad(
-            images,
-            pad_width=((0, 0), (ph, ph), (pw, pw)),
-            mode='constant',
-            constant_values=0
-        )
-    else:
-        padded_images = images
+    padded = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw)),
+        mode='constant'
+    )
 
-    # Compute output dimensions
-    out_h = (padded_images.shape[1] - kh) // sh + 1
-    out_w = (padded_images.shape[2] - kw) // sw + 1
+    h_out = (h + 2 * ph - kh) // sh + 1
+    w_out = (w + 2 * pw - kw) // sw + 1
 
-    # Initialize output array
-    output = np.zeros((m, out_h, out_w))
+    output = np.zeros((m, h_out, w_out))
 
-    # Convolution using two loops
-    for i in range(out_h):
-        for j in range(out_w):
-            vert_start = i * sh
-            vert_end = vert_start + kh
-            horiz_start = j * sw
-            horiz_end = horiz_start + kw
-            region = padded_images[
-                :, vert_start:vert_end, horiz_start:horiz_end
+    for i in range(h_out):
+        for j in range(w_out):
+            h_start = i * sh
+            h_end = h_start + kh
+            w_start = j * sw
+            w_end = w_start + kw
+
+            region = padded[
+                :, h_start:h_end, w_start:w_end
             ]
+
             output[:, i, j] = np.sum(
-                region * kernel,
+                region * kernel[np.newaxis, :, :],
                 axis=(1, 2)
             )
 
