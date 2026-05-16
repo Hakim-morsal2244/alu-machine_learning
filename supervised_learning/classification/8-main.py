50d454bfb371b517c286656c40faf4e5a10b6bf1
Hakim Morsal <m.hakim@alustudent.com>
2026-05-16 10:20:44 +0430

    Fix error

---
+++ supervised_learning/classification/8-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+NN = __import__('8-neural_network').NeuralNetwork
+
+lib = np.load('../data/Binary_Train.npz')
+X = lib['X'].reshape((lib['X'].shape[0], -1)).T
+
+np.random.seed(0)
+
+nn = NN(X.shape[0], 3)
+
+print(nn.W1)
+print(nn.W1.shape)
+print(nn.b1)
+print(nn.W2)
+print(nn.W2.shape)
+print(nn.b2)
+print(nn.A1)
+print(nn.A2)
+
+nn.A1 = 10
+print(nn.A1)
