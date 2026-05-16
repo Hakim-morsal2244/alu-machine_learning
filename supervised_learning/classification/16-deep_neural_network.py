13551750d02329087d35112c3a5eca896fb385a1
Hakim Morsal <m.hakim@alustudent.com>
2026-05-16 11:20:29 +0430

    Complete neural network tasks and clean repository

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Deep Neural Network for binary classification"""
+"""Deep Neural Network"""
 
 import numpy as np
 
@@ -8,43 +8,42 @@ class DeepNeuralNetwork:
     """Deep neural network"""
 
     def __init__(self, nx, layers):
-        """Initialize deep neural network"""
-
-        # ---------------- VALIDATION ORDER (IMPORTANT) ----------------
+        """Initialize the network"""
 
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
+        if (not isinstance(layers, list) or len(layers) == 0):
+            raise TypeError(
+                "layers must be a list of positive integers"
+            )
 
-        if len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
+        for x in layers:
+            if not isinstance(x, int) or x <= 0:
+                raise TypeError(
+                    "layers must be a list of positive integers"
+                )
 
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise TypeError("layers must be a list of positive integers")
-
-        # ---------------- ATTRIBUTES ----------------
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        # ---------------- INITIALIZATION ----------------
-        for l in range(1, self.L + 1):
+        for layer in range(1, self.L + 1):
 
-            if l == 1:
-                self.weights["W1"] = (
-                    np.random.randn(layers[0], nx) *
-                    np.sqrt(2 / nx)
-                )
+            if layer == 1:
+                rand = np.random.randn(layers[0], nx)
+                self.weights["W1"] = rand * np.sqrt(2 / nx)
             else:
-                self.weights["W{}".format(l)] = (
-                    np.random.randn(
-                        layers[l - 1],
-                        layers[l - 2]
-                    ) * np.sqrt(2 / layers[l - 2])
+                rand = np.random.randn(
+                    layers[layer - 1],
+                    layers[layer - 2]
+                )
+                self.weights["W{}".format(layer)] = (
+                    rand * np.sqrt(2 / layers[layer - 2])
                 )
 
-            self.weights["b{}".format(l)] = np.zeros((layers[l - 1], 1))
+            self.weights["b{}".format(layer)] = np.zeros(
+                (layers[layer - 1], 1)
+            )
