6efae6cea26d13a885fed034c8f45b253061c37b
Hakim Morsal <m.hakim@alustudent.com>
2026-05-16 09:46:39 +0430

    Fix error

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -8,6 +8,7 @@ class Neuron:
     """Neuron class for binary classification"""
 
     def __init__(self, nx):
+        """Initialize neuron"""
         if not isinstance(nx, int):
             raise TypeError("nx must be a integer")
         if nx < 1:
@@ -19,22 +20,44 @@ class Neuron:
 
     @property
     def W(self):
+        """Getter for W"""
         return self.__W
 
     @property
     def b(self):
+        """Getter for b"""
         return self.__b
 
     @property
     def A(self):
+        """Getter for A"""
         return self.__A
 
     def forward_prop(self, X):
+        """
+        Calculates forward propagation of the neuron
+
+        Args:
+            X (numpy.ndarray): input data (nx, m)
+
+        Returns:
+            numpy.ndarray: activated output (A)
+        """
         Z = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
 
     def cost(self, Y, A):
+        """
+        Calculates cost using logistic regression
+
+        Args:
+            Y (numpy.ndarray): correct labels (1, m)
+            A (numpy.ndarray): activated output (1, m)
+
+        Returns:
+            float: cost value
+        """
         m = Y.shape[1]
         cost = -(1 / m) * np.sum(
             Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
