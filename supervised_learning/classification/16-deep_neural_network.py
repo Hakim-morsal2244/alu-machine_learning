1d630edaa20d7088fed749116e5ca2fb08fe2bad
Hakim Morsal <m.hakim@alustudent.com>
2026-05-16 11:38:57 +0430

    Task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -20,33 +20,32 @@ class DeepNeuralNetwork:
                 "layers must be a list of positive integers"
             )
 
-        for x in layers:
-            if not isinstance(x, int) or x <= 0:
-                raise TypeError(
-                    "layers must be a list of positive integers"
-                )
+        if not all(
+            isinstance(x, int) and x > 0 for x in layers
+        ):
+            raise TypeError(
+                "layers must be a list of positive integers"
+            )
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        layer = 1
+        for layer in range(self.L):
 
-        self.weights["W1"] = (
-            np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
-        )
-        self.weights["b1"] = np.zeros((layers[0], 1))
-
-        while layer < self.L:
-            self.weights["W{}".format(layer + 1)] = (
-                np.random.randn(
-                    layers[layer],
-                    layers[layer - 1]
-                ) * np.sqrt(2 / layers[layer - 1])
-            )
+            if layer == 0:
+                self.weights["W1"] = (
+                    np.random.randn(layers[0], nx)
+                    * np.sqrt(2 / nx)
+                )
+            else:
+                self.weights["W{}".format(layer + 1)] = (
+                    np.random.randn(
+                        layers[layer],
+                        layers[layer - 1]
+                    ) * np.sqrt(2 / layers[layer - 1])
+                )
 
             self.weights["b{}".format(layer + 1)] = np.zeros(
                 (layers[layer], 1)
             )
-
-            layer += 1
