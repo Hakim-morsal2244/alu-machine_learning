15e3408482e1b062de9a6e223548d47f8a73d001
Hakim Morsal <m.hakim@alustudent.com>
2026-05-16 11:28:57 +0430

    Task new 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -8,14 +8,14 @@ class DeepNeuralNetwork:
     """Deep neural network"""
 
     def __init__(self, nx, layers):
-        """Initialize the network"""
+        """Initialize network"""
 
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if (not isinstance(layers, list) or len(layers) == 0):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError(
                 "layers must be a list of positive integers"
             )
@@ -30,20 +30,23 @@ class DeepNeuralNetwork:
         self.cache = {}
         self.weights = {}
 
-        for layer in range(1, self.L + 1):
+        layer = 1
 
-            if layer == 1:
-                rand = np.random.randn(layers[0], nx)
-                self.weights["W1"] = rand * np.sqrt(2 / nx)
-            else:
-                rand = np.random.randn(
-                    layers[layer - 1],
-                    layers[layer - 2]
-                )
-                self.weights["W{}".format(layer)] = (
-                    rand * np.sqrt(2 / layers[layer - 2])
-                )
+        self.weights["W1"] = (
+            np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
+        )
+        self.weights["b1"] = np.zeros((layers[0], 1))
 
-            self.weights["b{}".format(layer)] = np.zeros(
-                (layers[layer - 1], 1)
+        while layer < self.L:
+            self.weights["W{}".format(layer + 1)] = (
+                np.random.randn(
+                    layers[layer],
+                    layers[layer - 1]
+                ) * np.sqrt(2 / layers[layer - 1])
             )
+
+            self.weights["b{}".format(layer + 1)] = np.zeros(
+                (layers[layer], 1)
+            )
+
+            layer += 1
