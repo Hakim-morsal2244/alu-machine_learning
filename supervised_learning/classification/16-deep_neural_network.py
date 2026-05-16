000e92d0b26d2de48b4a1616ac548aff5bd2b477
Hakim Morsal <m.hakim@alustudent.com>
2026-05-16 11:44:55 +0430

    Task new 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,51 +1,49 @@
 #!/usr/bin/env python3
-"""Deep Neural Network"""
+""" Deep Neural Network
+"""
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Deep neural network"""
+    """ Class that defines a deep neural network performing binary
+        classification.
+    """
 
     def __init__(self, nx, layers):
-        """Initialize network"""
+        """ Instantiation function
 
+        Args:
+            nx (int): number of input features
+            layers (list): representing the number of nodes in each layer of
+                           the network
+        """
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError('nx must be a positive integer')
 
-        if not isinstance(layers, list) or len(layers) == 0:
-            raise TypeError(
-                "layers must be a list of positive integers"
-            )
-
-        if not all(
-            isinstance(x, int) and x > 0 for x in layers
-        ):
-            raise TypeError(
-                "layers must be a list of positive integers"
-            )
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+        if len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for layer in range(self.L):
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
 
-            if layer == 0:
-                self.weights["W1"] = (
-                    np.random.randn(layers[0], nx)
-                    * np.sqrt(2 / nx)
-                )
+            if i == 0:
+                # He et al. initialization
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
             else:
-                self.weights["W{}".format(layer + 1)] = (
-                    np.random.randn(
-                        layers[layer],
-                        layers[layer - 1]
-                    ) * np.sqrt(2 / layers[layer - 1])
-                )
+                # He et al. initialization
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
 
-            self.weights["b{}".format(layer + 1)] = np.zeros(
-                (layers[layer], 1)
-            )
+            # Zero initialization
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
--- supervised_learning/classification/16-main.py
+++ supervised_learning/classification/16-main.py
@@ -4,17 +4,14 @@ import numpy as np
 
 Deep = __import__('16-deep_neural_network').DeepNeuralNetwork
 
-lib = np.load('../data/Binary_Train.npz')
-X = lib['X'].reshape((lib['X'].shape[0], -1)).T
-Y = lib['Y']
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 np.random.seed(0)
-
 deep = Deep(X.shape[0], [5, 3, 1])
-
 print(deep.cache)
 print(deep.weights)
 print(deep.L)
-
 deep.L = 10
 print(deep.L)
