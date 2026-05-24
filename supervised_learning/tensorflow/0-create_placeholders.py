27e313c9f4c60f6eb57d02ea049c9b056f2365e1
Hakim-morsal2244 <m.hakim@alustudent.com>
2026-05-24 21:22:34 +0430

    Task 0: create placeholders

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -3,22 +3,11 @@
 
 import tensorflow as tf
 
-
-# Force TF1 behavior inside TF2
 tf.compat.v1.disable_eager_execution()
 
 
 def create_placeholders(nx, classes):
-    """
-    Creates placeholders for input data and labels
-
-    nx: number of input features
-    classes: number of classes
-
-    Returns:
-        x: input placeholder
-        y: label placeholder
-    """
+    """Creates placeholders for input data and labels"""
     x = tf.compat.v1.placeholder(tf.float32, shape=(None, nx), name="x")
     y = tf.compat.v1.placeholder(tf.float32, shape=(None, classes), name="y")
 
