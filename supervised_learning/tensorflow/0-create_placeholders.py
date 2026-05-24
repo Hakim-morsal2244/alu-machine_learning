37e2307fb4f0d297656fe493f1b53dafbfff1fda
Hakim-morsal2244 <m.hakim@alustudent.com>
2026-05-24 21:39:35 +0430

    Task 0: create placeholders

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Creates TensorFlow placeholders"""
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
-tf.compat.v1.disable_eager_execution()
+tf.disable_eager_execution()
 
 
 def create_placeholders(nx, classes):
     """Creates placeholders for input data and labels"""
-    x = tf.compat.v1.placeholder(tf.float32, shape=(None, nx), name="x")
-    y = tf.compat.v1.placeholder(tf.float32, shape=(None, classes), name="y")
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
 
     return x, y
