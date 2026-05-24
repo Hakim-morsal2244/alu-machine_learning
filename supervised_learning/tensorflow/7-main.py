b304208d198b926ced1cdf99a6a24bce313ece7e
Hakim-morsal2244 <m.hakim@alustudent.com>
2026-05-24 23:26:33 +0430

    Task 0: create placeholders

--- supervised_learning/tensorflow/7-main.py
+++ supervised_learning/tensorflow/7-main.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
+"""Evaluate MNIST model predictions and display sample results."""
 
-import matplotlib.pyplot as plt
-import numpy as np
 evaluate = __import__('7-evaluate').evaluate
+
 
 def one_hot(y, classes):
     """convert an array to a one-hot matrix"""
+    import numpy as np
     one_hot_matrix = np.zeros((y.shape[0], classes))
     one_hot_matrix[np.arange(y.shape[0]), y] = 1
     return one_hot_matrix
 
+
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import numpy as np
     lib = np.load('../data/MNIST.npz')
     X_test_3D = lib['X_test']
     Y_test = lib['Y_test']
     X_test = X_test_3D.reshape((X_test_3D.shape[0], -1))
-    Y_test_oh = one_hot(Y_test, 10)
+    y_test_oh = one_hot(Y_test, 10)
 
-    Y_pred_oh, accuracy, cost = evaluate(X_test, Y_test_oh, './model.ckpt')
+    Y_pred_oh, accuracy, cost = evaluate(X_test, y_test_oh, './model.ckpt')
     print("Test Accuracy:", accuracy)
     print("Test Cost:", cost)
 
