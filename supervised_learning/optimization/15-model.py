22c5de2b2a4b776ffc0f2c30043c91b8653c10a7
Hakim-morsal2244 <m.hakim@alustudent.com>
2026-05-31 20:08:33 +0430

    Add mini batch training function

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -1,57 +1,94 @@
 #!/usr/bin/env python3
-"""Builds, trains, and saves a neural network model"""
+"""15-model"""
 
-import numpy as np
 import tensorflow as tf
+import numpy as np
 
-shuffle_data = __import__('2-shuffle_data').shuffle_data
-create_batch_norm_layer = __import__('14-batch_norm').create_batch_norm_layer
+
+def shuffle_data(X, Y):
+    """shuffle data"""
+    p = np.random.permutation(X.shape[0])
+    return X[p], Y[p]
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """batch normalization layer"""
+    init = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG"
+    )
+
+    dense = tf.layers.Dense(
+        units=n,
+        activation=None,
+        kernel_initializer=init
+    )
+
+    Z = dense(prev)
+
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+
+    mean, variance = tf.nn.moments(Z, axes=[0])
+
+    Z_norm = tf.nn.batch_normalization(
+        Z,
+        mean,
+        variance,
+        beta,
+        gamma,
+        1e-8
+    )
+
+    if activation is None:
+        return Z_norm
+
+    return activation(Z_norm)
 
 
 def model(Data_train, Data_valid, layers, activations,
           alpha=0.001, beta1=0.9, beta2=0.999,
-          epsilon=1e-8, decay_rate=1, batch_size=32,
-          epochs=5, save_path='/tmp/model.ckpt'):
-    """
-    builds, trains, and saves a neural network model
-    """
+          epsilon=1e-8, decay_rate=1,
+          batch_size=32, epochs=5,
+          save_path='/tmp/model.ckpt'):
+    """builds and trains model"""
+
     X_train, Y_train = Data_train
     X_valid, Y_valid = Data_valid
 
     nx = X_train.shape[1]
     classes = Y_train.shape[1]
 
-    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
-    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    x = tf.placeholder(tf.float32, shape=[None, nx])
+    y = tf.placeholder(tf.float32, shape=[None, classes])
 
     layer = x
 
-    for i in range(len(layers)):
-        if i == len(layers) - 1:
-            init = tf.contrib.layers.variance_scaling_initializer(
-                mode="FAN_AVG"
-            )
-            dense = tf.layers.Dense(
-                units=layers[i],
-                activation=activations[i],
-                kernel_initializer=init
-            )
-            layer = dense(layer)
-        else:
-            layer = create_batch_norm_layer(
-                layer,
-                layers[i],
-                activations[i]
-            )
+    for nodes, activation in zip(layers[:-1], activations[:-1]):
+        layer = create_batch_norm_layer(
+            layer,
+            nodes,
+            activation
+        )
 
-    y_pred = layer
-
-    loss = tf.losses.softmax_cross_entropy(y, y_pred)
-
-    correct = tf.equal(
-        tf.argmax(y_pred, 1),
-        tf.argmax(y, 1)
+    init = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG"
     )
+
+    output = tf.layers.Dense(
+        units=layers[-1],
+        activation=activations[-1],
+        kernel_initializer=init
+    )(layer)
+
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=y,
+        logits=output
+    )
+
+    prediction = tf.argmax(output, axis=1)
+    labels = tf.argmax(y, axis=1)
+
+    correct = tf.equal(prediction, labels)
 
     accuracy = tf.reduce_mean(
         tf.cast(correct, tf.float32)
@@ -85,10 +122,75 @@ def model(Data_train, Data_valid, layers, activations,
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        m = X_train.shape[0]
-        steps = (m + batch_size - 1) // batch_size
+        train_cost, train_acc = sess.run(
+            [loss, accuracy],
+            feed_dict={
+                x: X_train,
+                y: Y_train
+            }
+        )
 
-        for epoch in range(epochs + 1):
+        valid_cost, valid_acc = sess.run(
+            [loss, accuracy],
+            feed_dict={
+                x: X_valid,
+                y: Y_valid
+            }
+        )
+
+        print("After 0 epochs:")
+        print("\tTraining Cost: {}".format(train_cost))
+        print("\tTraining Accuracy: {}".format(train_acc))
+        print("\tValidation Cost: {}".format(valid_cost))
+        print("\tValidation Accuracy: {}".format(valid_acc))
+
+        for epoch in range(epochs):
+
+            X_shuff, Y_shuff = shuffle_data(
+                X_train,
+                Y_train
+            )
+
+            m = X_train.shape[0]
+
+            step = 0
+
+            for i in range(0, m, batch_size):
+
+                X_batch = X_shuff[i:i + batch_size]
+                Y_batch = Y_shuff[i:i + batch_size]
+
+                sess.run(
+                    train_op,
+                    feed_dict={
+                        x: X_batch,
+                        y: Y_batch
+                    }
+                )
+
+                step += 1
+
+                if step % 100 == 0:
+
+                    batch_cost, batch_acc = sess.run(
+                        [loss, accuracy],
+                        feed_dict={
+                            x: X_batch,
+                            y: Y_batch
+                        }
+                    )
+
+                    print("\tStep {}:".format(step))
+                    print(
+                        "\t\tCost: {}".format(
+                            batch_cost
+                        )
+                    )
+                    print(
+                        "\t\tAccuracy: {}".format(
+                            batch_acc
+                        )
+                    )
 
             train_cost, train_acc = sess.run(
                 [loss, accuracy],
@@ -106,50 +208,33 @@ def model(Data_train, Data_valid, layers, activations,
                 }
             )
 
-            print("After {} epochs:".format(epoch))
-            print("\tTraining Cost: {}".format(train_cost))
-            print("\tTraining Accuracy: {}".format(train_acc))
-            print("\tValidation Cost: {}".format(valid_cost))
-            print("\tValidation Accuracy: {}".format(valid_acc))
-
-            if epoch == epochs:
-                break
-
-            X_shuff, Y_shuff = shuffle_data(
-                X_train,
-                Y_train
+            print(
+                "After {} epochs:".format(
+                    epoch + 1
+                )
+            )
+            print(
+                "\tTraining Cost: {}".format(
+                    train_cost
+                )
+            )
+            print(
+                "\tTraining Accuracy: {}".format(
+                    train_acc
+                )
+            )
+            print(
+                "\tValidation Cost: {}".format(
+                    valid_cost
+                )
+            )
+            print(
+                "\tValidation Accuracy: {}".format(
+                    valid_acc
+                )
             )
 
-            for step in range(steps):
-                start = step * batch_size
-                end = min(start + batch_size, m)
-
-                X_batch = X_shuff[start:end]
-                Y_batch = Y_shuff[start:end]
-
-                sess.run(
-                    train_op,
-                    feed_dict={
-                        x: X_batch,
-                        y: Y_batch
-                    }
-                )
-
-                step_num = step + 1
-
-                if (step_num % 100 == 0 or
-                        step_num == steps):
-
-                    batch_cost, batch_acc = sess.run(
-                        [loss, accuracy],
-                        feed_dict={
-                            x: X_batch,
-                            y: Y_batch
-                        }
-                    )
-
-                    print("\tStep {}:".format(step_num))
-                    print("\t\tCost: {}".format(batch_cost))
-                    print("\t\tAccuracy: {}".format(batch_acc))
-
-        return saver.save(sess, save_path)
+        return saver.save(
+            sess,
+            save_path
+        )
