479e74157773d4f15a0a5013d86430ee34ad5724
Hakim-morsal2244 <m.hakim@alustudent.com>
2026-05-31 20:47:14 +0430

    Add mini batch training function

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,16 +7,12 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     Creates a fully connected layer with L2 regularization
-
-    prev: tensor output of previous layer
-    n: number of nodes in new layer
-    activation: activation function
-    lambtha: L2 regularization parameter
-
-    Returns: output tensor of new layer
     """
 
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG"
+    )
+
     regularizer = tf.contrib.layers.l2_regularizer(lambtha)
 
     layer = tf.layers.Dense(
