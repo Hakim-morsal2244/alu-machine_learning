067e62bc89173a104a30d34f5adf4c740cb99fc1
Hakim-morsal2244 <m.hakim@alustudent.com>
2026-06-11 22:21:58 +0430

    Fix error analysis confusion matrix and dataset file naming

---
+++ supervised_learning/error_analysis/6-compare_and_contrast.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def compare_and_contrast():
+    return "B"
---
+++ supervised_learning/error_analysis/6-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+compare_and_contrast = __import__('6-compare_and_contrast').compare_and_contrast
+
+if __name__ == '__main__':
+    print(compare_and_contrast())
